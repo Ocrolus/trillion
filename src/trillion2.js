ab9761faca6180fa17d0645bc19fe6fc4ee686f7
Ivan Malopinsky <ivan@imsky.co>
2016-01-07 22:57:15 -0500

    stub out trillion rewrite

---
+++ src/trillion2.js
@@ -0,0 +1,12 @@
+function Trillion () {};
+
+module.exports = function (options={}) {
+  //todo: listeners
+  //todo: pagination
+  //todo: sorting
+  //todo: searching
+  //todo: filters
+  //todo: aggregations
+  const instance = new Trillion();
+  return instance;
+};
